daba8d14d500060b42162ccf3c32e5b25d5f915a
jdrelick-leafgroup <jesse.drelick@leafgroup.com>
2017-01-30 18:04:17 -0800

    Redux DevTools

--- build/1.js
+++ build/1.js
@@ -1,10 +1,10 @@
 webpackJsonp([1],{
 
-/***/ 577:
+/***/ 578:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-eval("\n\nObject.defineProperty(exports, \"__esModule\", {\n\tvalue: true\n});\nvar init = {\n\ttest: 'I am a dynamic reducer'\n};\n\nexports.default = function () {\n\tvar state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : init;\n\n\treturn state;\n};\n\n//////////////////\n// WEBPACK FOOTER\n// ./example/components/async/reducer.js\n// module id = 577\n// module chunks = 1\n\n//# sourceURL=webpack:///./example/components/async/reducer.js?");
+eval("\n\nObject.defineProperty(exports, \"__esModule\", {\n\tvalue: true\n});\nvar init = {\n\ttest: 'I am a dynamic reducer'\n};\n\nexports.default = function () {\n\tvar state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : init;\n\n\treturn state;\n};\n\n//////////////////\n// WEBPACK FOOTER\n// ./example/components/async/reducer.js\n// module id = 578\n// module chunks = 1\n\n//# sourceURL=webpack:///./example/components/async/reducer.js?");
 
 /***/ })
 
--- example/store.js
+++ example/store.js
@@ -1,10 +1,12 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import createSagaMiddleware from 'redux-saga'
+import root from 'window-or-global'
 
 import Reducer from './reducer'
 import Saga from './saga'
 
 const sagaMiddleware = createSagaMiddleware()
+const composeEnhancers = root.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 // createStore() is wrapped in a function so it can be executed on each server request
 // Otherwise store state persists across multiple requests for lifetime of Node process
@@ -12,7 +14,7 @@ export default (init = {}) => {
   const store = createStore(
     Reducer,
     init,
-    applyMiddleware(sagaMiddleware)
+    composeEnhancers(applyMiddleware(sagaMiddleware))
   )
   sagaMiddleware.run(Saga)
   return store
